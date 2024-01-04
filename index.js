79f05ad665aabe8dacf6de8045b52333a3c0e7da
hari-sqshft <hari@squareshift.co>
2024-01-04 16:27:59 +0530

    Changes for request URLs

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 const dotenv = require("dotenv")
 dotenv.config()
+const { APM_CLIENT_CONFIG } = require("./constants");
+const apm = require("elastic-apm-node").start(APM_CLIENT_CONFIG);
 
 const express = require("express");
 const cors = require("cors");
 const bodyparser = require("body-parser");
-const { APM_CLIENT_CONFIG } = require("./constants");
-const apm = require("elastic-apm-node").start(APM_CLIENT_CONFIG);
-
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
-
 const PORT = parseInt(process.env.PORT) || 8080;
-
 app.get("/", function (req, res) {
   res.status(200).send({
     success: true,
     data: "GET CALL",
   });
 });
-
 app.post("/", function (req, res) {
   var span = apm.startSpan("POST CALL STARTED");
   apm.startSpan("POST CALL STARTED");
@@ -32,7 +28,6 @@ app.post("/", function (req, res) {
     });
   }, 5000);
 });
-
 app.get("/error-call", function (req, res) {
   const error = new Error("Custom Error");
   apm.captureError(error, {
@@ -47,10 +42,8 @@ app.get("/error-call", function (req, res) {
     data: "ERROR CALL",
   });
 });
-
 app.post("/start-transaction", function (req, res) {
   var trans = apm.startTransaction("Custom Transaction", "Custom Type");
-//   apm.startSpan("POST CALL STARTED");
   setTimeout(() => {
     if (trans) trans.end();
     res.status(200).send({
@@ -59,7 +52,6 @@ app.post("/start-transaction", function (req, res) {
     });
   }, 5000);
 });
-
 app.listen(PORT, () => {
   console.log("Server Started on PORT " + PORT);
 });
